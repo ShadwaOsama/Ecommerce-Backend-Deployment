ed58d8d33d1bc3d01762d8c080f8d4c44eb4406a
ShadwaOsama <115946177+ShadwaOsama@users.noreply.github.com>
2024-07-07 02:30:04 +0300

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 
-const orderSchema = mongoose.Schema({
+const orderSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+        required: true, // Ensures that userId is always provided
     },
     orderItems: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'OrderItem',
-        required:true
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true, 
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+        }
     }],
- 
-    
     status: {
         type: String,
         required: true,
@@ -19,23 +25,37 @@ const orderSchema = mongoose.Schema({
     },
     totalPrice: {
         type: Number,
+        required: true,
+        default: 0, // Default value in case it's not provided
     },
-
     dateOrdered: {
         type: Date,
         default: Date.now,
     },
-})
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
 
+// Virtual for id
 orderSchema.virtual('id').get(function () {
     return this._id.toHexString();
 });
 
-orderSchema.set('toJSON', {
-    virtuals: true,
+// Pre-save hook to calculate totalPrice
+orderSchema.pre('save', async function(next) {
+    const Order = this;
+    let totalPrice = 0;
+
+    for (const item of Order.orderItems) {
+        const product = await mongoose.model('Product').findById(item.productId);
+        if (product) {
+            totalPrice += item.quantity * product.price;
+        }
+    }
+
+    Order.totalPrice = totalPrice;
+    next();
 });
 
 exports.Order = mongoose.model('Order', orderSchema);
-
-
-
